5c88ff94a2e75508307dd5eefc0826e60da9a8b6
Vinicius Teixeira <viniciusteixeiravini16@gmail.com>
2022-04-16 22:11:56 -0300

    Alterações Projeto time

--- Timer/assets/js/main.js
+++ Timer/assets/js/main.js
@@ -1,47 +1,51 @@
-const relogio = document.querySelector('.relogio');
+function watch(){
+    const relogio = document.querySelector('.relogio');
 
-let seconds = 0;
-let time;
+    let seconds = 0;
+    let time;
 
-function startClock(){
-    time = setInterval(function(){
-        seconds++;
-        relogio.innerHTML = getTimeSeconds(seconds);
-    }, 1000);
-    
-}
+    function startClock(){
+        time = setInterval(function(){
+            seconds++;
+            relogio.innerHTML = getTimeSeconds(seconds);
+        }, 1000);
+    }
 
-function getTimeSeconds(seconds){
-    let date = new Date(seconds * 1000);
-    return date.toLocaleTimeString('pt-BR', {
-        hours12: false,
-        timeZone: "UTC"
+    function getTimeSeconds(seconds){
+        let date = new Date(seconds * 1000);
+        return date.toLocaleTimeString('pt-BR', {
+            hours12: false,
+            timeZone: "UTC"
+        });
+    }
+
+    function stopClock(){
+        const stopTime = setTimeout(function(){
+            clearInterval(time);
+        }, 0);
+    }
+
+    document.addEventListener('click', function(e){
+        const el = e.target;
+
+        if(el.classList.contains('iniciar')){
+            clearInterval(time);
+            startClock();
+            relogio.style.color = 'black';
+        }
+
+        else if(el.classList.contains('pausar')){
+            stopClock();
+            relogio.style.color = "red";
+        }
+        else if(el.classList.contains('zerar')){
+            relogio.innerHTML = getTimeSeconds(0);
+            clearInterval(time);
+            seconds = 0;
+            relogio.style.color = "black";
+        }
     });
 }
 
-function stopClock(){
-    const stopTime = setTimeout(function(){
-        clearInterval(time);
-    }, 0);
-}
-
-document.addEventListener('click', function(e){
-    const el = e.target;
-
-    if(el.classList.contains('iniciar')){
-        clearInterval(time);
-        startClock();
-        relogio.style.color = 'black';
-    }
-
-    else if(el.classList.contains('pausar')){
-        stopClock();
-        relogio.style.color = "red";
-    }
-    else if(el.classList.contains('zerar')){
-        relogio.innerHTML = getTimeSeconds(0);
-        clearInterval(time);
-        seconds = 0;
-    }
-});
+watch();
 
